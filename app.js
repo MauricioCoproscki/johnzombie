cbe35697d5ce12beef53ad0958afdb4740eb1967
Mauricio Coproscki <mauricioafcoproscki@gmail.com>
2022-06-18 23:18:11 -0300

    criação do typescript

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("teste ok");
+"use strict";
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let button = document.getElementById("button");
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+if (button) {
+    button.addEventListener('click', () => {
+        if (input1 && input2) {
+            console.log(soma(Number(input1.value), Number(input2.value)));
+        }
+    });
+}
